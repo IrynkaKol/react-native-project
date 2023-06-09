cf9c0d5d6db5672ec2c71b52b366e7222368ac12
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-06-09 19:17:09 +0200

    add styles for button

--- Screens/auth/RegistrationScreen.js
+++ Screens/auth/RegistrationScreen.js
@@ -4,50 +4,106 @@ import {
   StyleSheet,
   TextInput,
   TouchableOpacity,
-  ImageBackground
+  ImageBackground,
+  Button,
 } from "react-native";
-import backgroundImage from '../../assets/images/background.png'
+import backgroundImage from "../../assets/images/background.png";
 
 export const RegistrationScreen = ({}) => {
   return (
-    <ImageBackground source={backgroundImage}
-    resizeMode="cover"
-    style={{flex: 1,
-        justifyContent: 'center',}}
+    <ImageBackground
+      source={backgroundImage}
+      resizeMode="cover"
+      style={styles.background}
     >
-        <View style={styles.container}> 
-      <View>
-        <Text>Реєстрація</Text>
-      </View>
-      <View>
-        <TextInput placeholder="Логін" />
-        <TextInput placeholder="Адреса електронної пошти" />
-        <TextInput placeholder="Пароль" />
-      </View>
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonTitle}>Зареєструватися</Text>
-      </TouchableOpacity>
+      <View style={styles.container}>
+        <View>
+          <Text style={styles.title}>Реєстрація</Text>
+        </View>
+        <View style={styles.formContiner}>
+          <TextInput style={styles.inputForm} placeholder="Логін" />
+          <TextInput
+            style={styles.inputForm}
+            placeholder="Адреса електронної пошти"
+          />
+
+          <View style={styles.passwordContainer}>
+            <TextInput style={styles.inputForm} placeholder="Пароль" secureTextEntry={true} />
+            <TouchableOpacity style={styles.passswordButton}>
+              <Text>Показати</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonTitle}>Зареєструватися</Text>
+        </TouchableOpacity>
+        
+        <TouchableOpacity>
+          <Text>Вже є акаунт? Увійти</Text>
+        </TouchableOpacity>
       </View>
     </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1, 
+    justifyContent: "center"
+  },
+
   container: {
-    backgroundColor: '#fff',
-    
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#fff",
+    // justifyContent: 'center',
+    // alignItems: 'center',
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
     marginTop: 263,
-    minHeight: 549
-
-
-       
+    minHeight: 549,
+    paddingTop: 92,
+    paddingBottom: 78,
+    paddingHorizontal: 16,
   },
-  button: {},
+  title: {
+    textAlign: "center",
+    marginBottom: 33,
+    fontSize: 30
+  },
+  formContiner: {
+    gap: 16,
+    marginBottom: 32,
+  },
+
+  inputForm: {
+    height: 50,
+    padding: 16,
+    borderWidth: 1,
+    borderColor: "#E8E8E8",
+    borderRadius: 8,
+    backgroundColor: "#F6F6F6",
+    fontSize: 16,
+   
+  },
+  button: {
+backgroundColor:'#FF6C00',
+borderRadius: 100,
+paddingVertical: 16,
+marginBottom: 16,
+  },
   buttonTitle: {
     fontSize: 16,
+    color: '#FFFFFF',
+    textAlign:'center',
+    
+   
+    
+  },
+  passwordContainer: {
+    position: "relative",
+  },
+  passswordButton: {
+    position: "absolute",
+    top: 16,
+    right: 15,
   },
 });

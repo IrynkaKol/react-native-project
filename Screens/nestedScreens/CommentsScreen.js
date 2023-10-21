691b581afbf0a4bf3e96ab50cdbe1a548d1797b6
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-10-21 23:37:02 +0200

    update comments

--- Screens/nestedScreens/CommentsScreen.js
+++ Screens/nestedScreens/CommentsScreen.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import {
   View,
@@ -6,38 +6,80 @@ import {
   StyleSheet,
   TouchableOpacity,
   TextInput,
+  SafeAreaView,
+  FlatList,
 } from "react-native";
 import {
-    collection,
-    addDoc,
-    onSnapshot,
-    doc,
-    query,
-    orderBy,
-  } from 'firebase/firestore';
+  collection,
+  addDoc,
+  getDocs,
+  onSnapshot,
+  doc,
+  query,
+  orderBy,
+} from "firebase/firestore";
 import { AntDesign } from "@expo/vector-icons";
-import {db} from "../../firebase/config";
+import { db } from "../../firebase/config";
 import { useSelector } from "react-redux";
 
-export const CommentsScreen = ({route}) => { // із route дістаємо із HOME  id item
-    const {postId} = route.params;
+export const CommentsScreen = ({ route }) => {
+  // із route дістаємо із HOME  id item
+  const { postId } = route.params;
   const [comment, setComment] = useState("");
-  const {login} = useSelector(state => state.auth)
+  const [allComments, setAllComments] = useState([]);
+  const { login } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    getAllPosts();
+  }, []);
 
   const createPost = async () => {
-    const postRef = doc(db, 'posts', postId);
-    const commentsCollectionRef = collection(postRef, 'comments');
+    const postRef = doc(db, "posts", postId);
+    const commentsCollectionRef = collection(postRef, "comments");
 
-      const commentRef = await addDoc(commentsCollectionRef, {
-        comment,
-        login,
-      });
-      // console.log('commentRef', commentRef)
+    const commentRef = await addDoc(commentsCollectionRef, {
+      comment,
+      login,
+    });
+    // console.log('commentRef', commentRef)
   };
 
+  const getAllPosts = async () => {
+    const postRef = doc(db, "posts", postId);
+
+    onSnapshot(
+      query(collection(postRef, "comments"), orderBy("login")),
+      (snapshot) =>
+        setAllComments(
+          snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+        )
+    );
+  };
+  // postRef = doc(db, "posts", postId);
+  // const commentsCollectionRef = collection(postRef, "comments");
+  // onSnapshot(commentsCollectionRef, (snapshot) => {
+  //   const postsData = snapshot.docs.map((doc) => ({
+  //     ...doc.data(),
+  //     id: doc.id,
+  //   }));
+  //    console.log("postsData", postsData); // Перевірка отриманих даних
+  //   setAllComments(postsData);
+  // });
 
   return (
     <View style={styles.container}>
+      <SafeAreaView style={styles.infoUserCommentThumb}>
+        <FlatList
+          data={allComments}
+          renderItem={({ item }) => (
+            <View style={[styles.infoComment]}>
+              <Text>{item.login}</Text>
+              <Text style={styles.commentText}>{item.comment}</Text>
+            </View>
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      </SafeAreaView>
       <View style={styles.inputWrapper}>
         <TextInput
           style={styles.inputComment}
@@ -61,13 +103,34 @@ export const CommentsScreen = ({route}) => { // із route дістаємо із
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-end',
+    justifyContent: "flex-end",
     marginTop: 32,
     marginBottom: 16,
     paddingHorizontal: 16,
   },
+  infoUserCommentThumb: {
+    flexDirection: "row-reverse",
+    gap: 16,
+    marginBottom: 24,
+  },
+  commentText: {
+    marginBottom: 8,
+    fontFamily: "Roboto-Regular",
+    fontSize: 13,
+    lineHeight: 18,
+    color: "#212121",
+  },
   inputWrapper: {
     position: "relative",
+  },
+  infoComment: {
+    width: "85%",
+    height: "auto",
+    padding: 16,
+    borderRadius: 6,
+    borderWidth: 1,
+    borderColor: '#20b2AA',
+    backgroundColor: "rgba(0, 0, 0, 0.03)",
   },
   inputComment: {
     paddingVertical: 16,
